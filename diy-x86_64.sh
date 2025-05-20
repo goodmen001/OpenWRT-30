ac80c42b95c9ac55e004b55d9024edc6aca69cd2
NERD <168670760+zhiern@users.noreply.github.com>
2025-05-20 14:19:23 +0800

    Update diy-x86_64.sh

--- diy-x86_64.sh
+++ diy-x86_64.sh
@@ -358,9 +358,6 @@ git clone https://$github/sbwml/openwrt_helloworld package/new/helloworld -b v5
 rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
 git clone https://$github/sbwml/openwrt-alist package/new/alist
 
-# adguardhome
-git clone --depth=1 -b lua https://github.com/sirpdboy/luci-app-adguardhome package/new/luci-app-adguardhome
-
 # netdata
 sed -i 's/syslog/none/g' feeds/packages/admin/netdata/files/netdata.conf
 
@@ -399,6 +396,7 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/new/lucky
 
 # pkgs
 git clone https://github.com/sbwml/openwrt_pkgs package/new/openwrt_pkgs
+rm -rf package/new/openwrt_pkgs/luci-app-adguardhome
 
 # autocore-arm
 git clone https://github.com/sbwml/autocore-arm package/new/autocore-arm
