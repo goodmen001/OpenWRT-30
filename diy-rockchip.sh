8dbb69e282d185085a4490fca49a7e18bdc8df26
NERD <168670760+zhiern@users.noreply.github.com>
2025-05-20 14:10:15 +0800

    Update diy-rockchip.sh

--- diy-rockchip.sh
+++ diy-rockchip.sh
@@ -379,6 +379,9 @@ git clone https://$github/sbwml/luci-app-mosdns -b v5 package/new/mosdns
 # OpenAppFilter
 git clone https://$github/sbwml/OpenAppFilter --depth=1 package/new/OpenAppFilter
 
+# adguardhome
+git clone --depth=1 -b lua https://github.com/sirpdboy/luci-app-adguardhome package/new/luci-app-adguardhome
+
 # nlbwmon
 sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
 sed -i 's/services/network/g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
