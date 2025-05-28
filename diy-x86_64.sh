b04bd5f1688cf3bdbff200e387845cbd9d44552f
NERD <168670760+zhiern@users.noreply.github.com>
2025-05-29 01:39:11 +0800

    Update diy-x86_64.sh

--- diy-x86_64.sh
+++ diy-x86_64.sh
@@ -368,7 +368,7 @@ git clone https://$github/sbwml/openwrt-alist package/new/alist
 
 # luci-app-sqm
 rm -rf feeds/luci/applications/luci-app-sqm
-git clone https://$github/zhiern/luci-app-sqm feeds/luci/applications/luci-app-sqm
+git clone $gitea/zhao/luci-app-sqm feeds/luci/applications/luci-app-sqm
 
 # netdata
 sed -i 's/syslog/none/g' feeds/packages/admin/netdata/files/netdata.conf
