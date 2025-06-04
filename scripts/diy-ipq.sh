d2014f674662fa32952fec4592c13649874f5357
NERD <168670760+zhiern@users.noreply.github.com>
2025-06-04 08:42:07 +0800

    Update diy-ipq.sh

--- scripts/diy-ipq.sh
+++ scripts/diy-ipq.sh
@@ -17,7 +17,7 @@ export github="github.com"
 sed -i "s/192.168.1.1/10.0.0.1/g" package/base-files/files/bin/config_generate
 
 # 修改名称
-sed -i 's/LibWRT/ZeroWrt/' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/ZeroWrt/' package/base-files/files/bin/config_generate
 
 # banner
 curl -s $mirror/Customize/base-files/banner > package/base-files/files/etc/banner
